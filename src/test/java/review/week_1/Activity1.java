dc76721412dd52cacff6044c36148c99e780bc7a
Jawid <jawidgulzai9@gmail.com>
2024-07-11 18:34:57 -0700

    some codes updated

--- src/test/java/review/week_1/Activity1.java
+++ src/test/java/review/week_1/Activity1.java
@@ -64,25 +64,6 @@ public class Activity1 {
         boolean profile = profileImageElement.isDisplayed();
         System.out.println(profile);
 
-//        Thread.sleep(3000);
-//        By profileImageLocator1 = By.id("profileImage");
-//        WebElement profileImageElement1 = driver.findElement(profileImageLocator1);
-//        boolean isProfileImageElementIsDisplayed = profileImageElement1.isDisplayed();
-//
-//
-//
-//        boolean profilePicture = driver.findElement(By.id("profileImage")).isDisplayed();
-//        if (profilePicture) System.out.println("Test Passed...");
-//        else System.out.println("Test Failed...");
-//
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        WebElement profileImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("profileImage")));
-//        profileImage.isDisplayed();
-//        if(profileImage.isDisplayed()){
-//            System.out.println("pic is present");
-//        }else { System.out.println("pic is not presnt ");
-//        }
-
         driver.quit();
 
     }
