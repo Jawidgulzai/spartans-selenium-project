acef1734deb5825ff8100017b1ccbd4261fe94ca
Jawid <jawidgulzai9@gmail.com>
2024-07-11 18:33:47 -0700

    some codes updated

--- src/test/java/review/week_1/Activity1.java
+++ src/test/java/review/week_1/Activity1.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Activity1 {
     public static void main(String[] args) throws InterruptedException {
@@ -33,9 +37,14 @@ public class Activity1 {
         WebElement inputNameElement = driver.findElement(inputNameLocator);
         inputNameElement.sendKeys("Jawid");
 
+        String emailPrefix = "jawid_spartans";
+        int number = (int) (Math.random() * 100);
+        String randomEmail = emailPrefix + number + "@gmail.com";
+        inputNameElement.sendKeys(randomEmail);
+
         By inputEmailLocator = By.id("emailInput");
         WebElement inputEmailElement = driver.findElement(inputEmailLocator);
-        inputEmailElement.sendKeys("Jawid444@gmail.com");
+        inputEmailElement.sendKeys(randomEmail);
 
         By inputPasswordLocator = By.id("passwordInput");
         WebElement inputPasswordElement = driver.findElement(inputPasswordLocator);
@@ -55,10 +64,26 @@ public class Activity1 {
         boolean profile = profileImageElement.isDisplayed();
         System.out.println(profile);
 
+//        Thread.sleep(3000);
+//        By profileImageLocator1 = By.id("profileImage");
+//        WebElement profileImageElement1 = driver.findElement(profileImageLocator1);
+//        boolean isProfileImageElementIsDisplayed = profileImageElement1.isDisplayed();
+//
+//
+//
+//        boolean profilePicture = driver.findElement(By.id("profileImage")).isDisplayed();
+//        if (profilePicture) System.out.println("Test Passed...");
+//        else System.out.println("Test Failed...");
+//
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        WebElement profileImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("profileImage")));
+//        profileImage.isDisplayed();
+//        if(profileImage.isDisplayed()){
+//            System.out.println("pic is present");
+//        }else { System.out.println("pic is not presnt ");
+//        }
+
         driver.quit();
-
-
-
 
     }
 }
---
+++ src/test/java/review/week_1/RandomNumber.java
@@ -0,0 +1,13 @@
+package review.week_1;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+
+//        double random = Math.random();
+//        int number = (int) (random * 100000);
+//        System.out.println(number);
+
+        int number = (int) (Math.random() * 10000);
+
+    }
+}
