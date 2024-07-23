a937ecf43337fb322abb3b357a217f2c1aaca53a
Jawid <jawidgulzai9@gmail.com>
2024-07-22 18:04:15 -0700

    some codes updated

--- src/test/java/utilities/Activity3.java
+++ src/test/java/utilities/Activity3.java
@@ -9,23 +9,22 @@ import org.openqa.selenium.support.ui.Select;
 import java.time.Duration;
 import java.util.List;
 
-public class Activity2 {
+public class Activity3 {
 
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://retail.tekschool-students.com/selenium/dropdown");
+        driver.get("https://tek-retail-ui.azurewebsites.net/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 
-        WebElement element = driver.findElement(By.id("languageSelect"));
-        Select webSideLanguageSelect = new Select(element);
-       // webSideLanguageSelect.selectByValue("Italian");
+        driver.findElement(By.xpath("//p[text()='Accessories']")).click();
+        driver.findElement(By.xpath("((//img[@class='image'])[3]")).submit();
 
-        List<WebElement> options = webSideLanguageSelect.getOptions();
-        int size = options.size();
-        webSideLanguageSelect.selectByIndex(size -1);
+        WebElement element = driver.findElement(By.className("product__select"));
+        Select keyboardElement = new Select(element);
+        keyboardElement.selectByValue("5");
 
     }
 }
